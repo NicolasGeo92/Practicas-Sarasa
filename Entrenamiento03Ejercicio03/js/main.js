fe5fe7fc27920bbc889d9e48d108afbd5e691cdd
NicolasGeo92 <nfg.1992@gmail.com>
2025-06-08 02:51:42 -0300

    Piedra, papel o tijera

--- Entrenamiento03Ejercicio03/js/main.js
+++ Entrenamiento03Ejercicio03/js/main.js
@@ -52,12 +52,120 @@
 //   { name: "Hungría", abbreviation: "HU", continent: "Europa", flag: "🇭🇺", language: "Húngaro" }
 // ];
 
-function cambiarBackground() {
-    const cambiarBackground = document.querySelector(".pais");
 
-    cambiarBackground.addEventListener("click", function () {
-        document.querySelector(".pais").style.backgroundColor = "red";
-    });
+function cambiarBackground() {
+    //No se puede usar addEventListener con un nodeList (querySelectorAll).
+    const cambiarBackgroundDelArticle = document.querySelectorAll(".pais");
+
+    for (let i = 0; i < cambiarBackgroundDelArticle.length; i++) {
+        cambiarBackgroundDelArticle[i].addEventListener("mouseover", function () {
+            //document.querySelectorAll(".pais")[i].style.backgroundColor = "red";
+            this.style.backgroundColor = "red";
+            //El this hace referencia al elemento que disparó el evento.
+        });
+    }
+
+    for (let i = 0; i < cambiarBackgroundDelArticle.length; i++) {
+        cambiarBackgroundDelArticle[i].addEventListener("mouseout", function () {
+            this.style.backgroundColor = "aqua";
+            //El this hace referencia al elemento que disparó el evento.
+        });
+    }
 }
 
 cambiarBackground();
+
+function jugandoAlPiedraPapelOTijera() {
+    let piedraPapelTijera = new Array("piedra", "papel", "tijera");
+    let mensaje;
+    let aleatorio;
+
+    let ganados = 0;
+    let perdidos = 0;
+
+    do {
+        let mensaje = prompt("Elija entre piedra, papel o tijera al mejor de 5");
+
+        let aleatorio = Math.random() * piedraPapelTijera.length;
+        aleatorio = Math.floor(aleatorio);
+
+        if (mensaje.toLowerCase() === "piedra") {
+            switch (piedraPapelTijera[aleatorio]) {
+                case "tijera":
+                    console.log("----------------------------------------------------");
+                    console.log("¡Ganaste!");
+                    ganados++;
+                    break;
+                case "papel":
+                    console.log("----------------------------------------------------");
+                    console.log("Perdiste");
+                    perdidos++;
+                    break;
+                case "piedra":
+                    console.log("----------------------------------------------------");
+                    console.log("Empate");
+                    break;
+            }
+        }
+
+        if (mensaje.toLowerCase() === "papel") {
+            switch (piedraPapelTijera[aleatorio]) {
+                case "piedra":
+                    console.log("----------------------------------------------------");
+                    console.log("¡Ganaste!");
+                    ganados++;
+                    break;
+                case "tijera":
+                    console.log("----------------------------------------------------");
+                    console.log("Perdiste");
+                    perdidos++;
+                    break;
+                case "papel":
+                    console.log("----------------------------------------------------");
+                    console.log("Empate");
+                    break;
+            }
+        }
+
+        if (mensaje.toLowerCase() === "tijera") {
+            switch (piedraPapelTijera[aleatorio]) {
+                case "papel":
+                    console.log("----------------------------------------------------");
+                    console.log("¡Ganaste!");
+                    ganados++;
+                    break;
+                case "piedra":
+                    console.log("----------------------------------------------------");
+                    console.log("Perdiste");
+                    perdidos++;
+                    break;
+                case "tijera":
+                    console.log("----------------------------------------------------");
+                    console.log("Empate");
+                    break;
+            }
+        } 
+    
+        if ((mensaje.toLowerCase() !== piedraPapelTijera[0]) && (mensaje.toLowerCase() !== piedraPapelTijera[1]) && (mensaje.toLowerCase() !== piedraPapelTijera[2])) {
+            console.log("----------------------------------------------------");
+            console.log("Escribiste cualquier cosa");
+        }
+
+        console.log("Elegiste: " + mensaje);
+        console.log("La IA eligió: " + piedraPapelTijera[aleatorio]);
+        console.log("Ganados: " + ganados);
+        console.log("Perdidos: " + perdidos);
+
+    } while (ganados !== 3 && perdidos !== 3);
+}
+
+jugandoAlPiedraPapelOTijera();
+
+//piedra > tijera
+//piedra < papel
+
+//tijera > papel
+//tijera < piedra
+
+//papel > piedra
+//papel < tijera
