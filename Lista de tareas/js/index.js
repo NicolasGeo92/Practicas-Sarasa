1cf48a1f49680f94cd70e43d700da7256986a688
NicolasGeo92 <nfg.1992@gmail.com>
2025-07-01 13:27:22 -0300

    Tareas hechas

--- Lista de tareas/js/index.js
+++ Lista de tareas/js/index.js
@@ -10,9 +10,33 @@ function cargarTareas() {
 
 // 3. Mostrar tareas en el HTML
 function mostrarTareas() {
-    
+    const lista = document.querySelector("#listaTareas");
+    lista.innerHTML = "";
+
+    tareas.forEach(function (tarea, i) {
+        const li = document.createElement("li");
+        li.innerHTML = `${tarea} <button onclick="eliminarTarea(${i})">O</button>`;
+        lista.appendChild(li);
+    });
 }
 
 // 4. Añadir nueva tarea
+function agregarTarea() {
+    const input = document.getElementById("nuevaTarea");
+    const texto = input.value.trim();
+
+    if (texto) {
+        tareas.push(texto);
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+        input.value = "";
+        mostrarTareas();
+    }
+}
 
 // 5. Eliminar tarea
+function eliminarTarea(index) {
+    tareas.splice(index, 1);
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    mostrarTareas();
+}
+cargarTareas();
