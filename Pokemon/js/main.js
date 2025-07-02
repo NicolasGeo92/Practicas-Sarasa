aa50cf5fd415af3e2df0944a8749eb3e5b602bcd
NicolasGeo92 <nfg.1992@gmail.com>
2025-07-01 22:56:28 -0300

    Estudiando

---
+++ Pokemon/js/main.js
@@ -0,0 +1,46 @@
+const modal = document.querySelector(".modal");
+const pokemonSeleccionado = document.querySelector("p")
+const aceptar = document.querySelector(".boton");
+
+const pokemonElegido = document.querySelectorAll("[name='pokemon']");
+let botonCombatir = document.getElementById("btn-combatir");
+
+const pokemons = [{ nombre: "Pikachu", tipo: "Electrico" },
+{ nombre: "Charmander", tipo: "Fuego" },
+{ nombre: "Milo", tipo: "Peludo" },
+{ nombre: "Homero", tipo: "Intenso" }
+]
+
+function verPokemones() {
+    const pokemones = document.querySelectorAll(".ver-pokemon");
+
+    for (let i = 0; i < pokemones.length; i++) {
+        pokemones[i].addEventListener("click", function () {
+            modal.style.display = "grid";
+            pokemonSeleccionado.textContent = (`${pokemons[i].nombre}  -  ${pokemons[i].tipo}`);
+        });
+    };
+}
+
+function botonAceptar() {
+    aceptar.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+}
+
+function habilitarBoton() {
+    for (let i = 0; i < pokemonElegido.length; i++) {
+        pokemonElegido[i].addEventListener("change", function () {
+            if (pokemonElegido[i].checked) {
+                botonCombatir.disabled = false;
+            }
+        });
+    }
+}
+
+console.log(botonCombatir);
+console.log(pokemonElegido);
+
+habilitarBoton();
+botonAceptar();
+verPokemones();
